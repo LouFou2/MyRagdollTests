c1da3a49e25de6e6496097500b1061913b6da17c
LouFou2 <parkipos@gmail.com>
2023-10-02 17:50:43 +1100

    Started Raycast Script

--- Assets/Scripts/FootRaycast.cs
+++ Assets/Scripts/FootRaycast.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class FootRaycast : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private CapsuleCollider _legCapsule = null;
+    private Rigidbody _rB;
+    private float _capsuleHeight = 0f;
+    [SerializeField] private float _springTip = 0.03f;
+    [SerializeField] private float springConstant = 100f;
+    private LayerMask _floorLayer;
     void Start()
     {
-        
-    }
+        _legCapsule = GetComponent<CapsuleCollider>();
+        _rB = GetComponent<Rigidbody>();
 
-    // Update is called once per frame
+        if (_legCapsule != null)
+        {
+            _capsuleHeight = _legCapsule.height;
+        }
+        _floorLayer = LayerMask.NameToLayer("Floor");
+    }
     void Update()
     {
+        Ray ray = new Ray(transform.position, transform.up);
+        Debug.DrawRay(ray.origin, ray.direction * (_capsuleHeight + 0.1f));
         
+        RaycastHit hitData;
+        
+        if (Physics.Raycast(ray, out hitData, (_capsuleHeight + 1), _floorLayer)) 
+        {
+            float _rayHitDistance = hitData.distance;
+            float _overShot = _rayHitDistance - (_capsuleHeight + _springTip);
+
+            if (_overShot < 0) 
+            {
+                // Calculate the upward force based on how much the object is below the spring tip
+                float springForce = Mathf.Abs(_overShot) * springConstant;
+
+                // Apply the upward force to the rigidbody
+                _rB.AddForce(-transform.up * springForce);
+            }
+        }  
     }
 }
